24ac328e86edf0bef456530612d69048da9ba89e
Edwin Reule <edwinreule@gmail.com>
2025-08-25 14:30:40 -0300

    Add Firebase

---
+++ src/firebase/firebase-config.ts
@@ -0,0 +1,17 @@
+const FirebaseData = {
+  type: 'service_account',
+  project_id: 'houser-admin',
+  private_key_id: 'fd9df848add049bc17f113e4b640535ca773f4fa',
+  private_key:
+    '-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDUVAvQP5blkxf8\nLwGZ4FRS+Jm4oiJw6U1zOhAyAjvKLseXIr5FWbLx7dVZu32N+oKerN+CSa8b5IlJ\nIISYRJh5eFa9AlD2dOtCiBOznWdIBRewqxiNW+djk8HnS+hZrj4g/4blpD5S57Ih\n4cYFF+L9K++vdZNUrqaX5iKGS4NrwGvllleEUtZf4iam+jozjhoytnuCovy/X1HN\nw/E1UhPbPCUxAtpvdet8bZa1Auk6Wrf8ihpshblnrX2gtQ/dVSUD/m0ZydoXDh6f\n5BbYyx+41nUfnsj9O03RBifKL+w8DBgJKH4nCEUXyhIoPegGVZcn8VbWxvY/dW1R\nIOmKZcPDAgMBAAECgf9X6nT5xVq1tDI6OTxwenJv6H+982RPXEq1a1VJX6NiE/yQ\nl9Iw5PMzOUcd4IQcRZ3QFcR2LgxTgE1X+h7s7Xo00Z8Yo9ziL4c6BR4jQQmCyJkG\njdGctnEYyhV/r+6FM6CGCUfRzjAAI++eF93mPf2+1nJoNEbMyYQxYRjqglnoQwNG\nIZTD/kHql29VEMVNANnJ0p4cgFhP3pbLbc8ROgsrZ3cYVGfoBCygmmhBIC4IJNV2\n99CD9ZrFW3L4AO30VB5aDOH7bek6Vrrk+1bxAtLkydZyClCyGn9nLPEouBy7q7hh\n0T1pFT+I2ehE1jsQZMbIWMxm+OUHFh7uaLlfDZECgYEA72ryYarmme+sltmZzSbO\nP4pJmkCEmKMrMdjiDkyKlJGhNtzM42Yf/WLSfAYaub8sJmpJB3hjoPTrylXpxJfw\nesSLMKNZbpLla8bfjwL45FxMERaGM2gbaB6QyDMb7KNnbyk2zK15DhzWWp8JOsQj\nz7STaxutys/4BD3uqaRIHw8CgYEA4wjIiWtbRlEtd9XUGg1LkEsGdydD7jiqtBam\nd3JajNIvIkFipFpt4uKYmwb6dSN07kpHpRtSG4PBK0J0mKfJ7Do2St/zF9DWr96L\nNehLjbRrpK2d/hGywD9Suo48Zlpf8TMskYdFK9QSC8YzeF2q60BaGjk8rdnMv/nD\n/M6A0A0CgYEAkIQGsIufTJYOoLPSkbH11wbbLL/7CcOcRCd9NFJhaBKLtHkNN8oi\nTynCkdiBc1OBC+MmF9tEkBim52h6hg+Muh4/QmmnhwYmc6sEsOeDYxyPUdU9xnP1\n7yBcFmw7U9jCi6feCyg/JRSlEuhigtV4JF0EiyeW/RARzkWv9ThTzGMCgYEAu4gn\nzYsGLAEl45SWzKYqwpeWUZn3fLEGIBeNjaDBtwxurob/6e5gUkcF8xhCjbup/KMS\npwj4O44LY6ZTFT2ThzNWYWa3L/brPv4+U1r7/aMIP1DB7nnEAxTLSxnuPkVsdpnk\nuJjZxCg6d8tR/Jyyb/wJ8RtSSfBcNj8japFQyYkCgYABQJpDWoNt97uRjluRoxG2\nEk044utloXQFIDXYlLHM65XIPi73aXDmYXJXicdE1qD12A5mIdkyrSPaMqL6W5rZ\nFBl0HdfEqSmsKYL4O6QSUEFQvspQQSWGIf7o2mk26Kdi8NOHjW+eA1HoBw7VIOME\n0tTs+l2lxGDgGyv0dWTLpg==\n-----END PRIVATE KEY-----\n',
+  client_email: 'firebase-adminsdk-fbsvc@houser-admin.iam.gserviceaccount.com',
+  client_id: '101047155343006820359',
+  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+  token_uri: 'https://oauth2.googleapis.com/token',
+  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+  client_x509_cert_url:
+    'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40houser-admin.iam.gserviceaccount.com',
+  universe_domain: 'googleapis.com',
+};
+
+export default FirebaseData;
