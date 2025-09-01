ee68c84e18f6a1804da5f81e0b68af9be74021ec
Edwin Reule <edwinreule@gmail.com>
2025-09-01 18:08:32 -0400

    fix: remove duplicate prisma module import

--- src/waiting-list/waiting-list.module.ts
+++ src/waiting-list/waiting-list.module.ts
@@ -1,14 +1,11 @@
 // Dependencies
 import { Module } from '@nestjs/common';
-import { PrismaModule } from '../prisma/prisma.module';
 
 // Services
-import { WaitingListService } from './waiting-list.service';
-
-// Modules
 import { PrismaModule } from '../prisma/prisma.module';
 
-// Controllers
+// Local
+import { WaitingListService } from './waiting-list.service';
 import { WaitingListController } from './waiting-list.controller';
 
 @Module({
